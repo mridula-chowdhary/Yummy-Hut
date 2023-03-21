ef7b4fa216e8e9db66a94266b44333f197b82ffa
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2023-03-21 14:43:49 +0530

    Added header component

---
+++ yummy-hut/src/Components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../Assets/meals.jpg';
+
+const Header = props => {
+return (
+<Fragment>
+<header className={classes.header}>
+    <h1> Yummy Hut </h1>
+    <button>Cart</button>
+</header>
+<div className={classes['main-image']}>
+    <img src ={mealsImage} alt ='A table full of food'></img>
+</div>
+</Fragment>
+);
+}
+
+export default Header;
