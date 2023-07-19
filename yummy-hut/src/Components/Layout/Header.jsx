237cd7600caea1060fc7579271fb511d32c528aa
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2023-07-19 16:01:30 +0530

    Made Changes in Index.js and index.css

--- yummy-hut/src/Components/Layout/Header.jsx
+++ yummy-hut/src/Components/Layout/Header.jsx
@@ -1,19 +1,20 @@
-import React,{Fragment} from 'react';
-import classes from './Header.module.css';
+import { Fragment } from 'react';
+import HeaderCartButtons from './HeaderCardButtons';
 import mealsImage from '../Assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props => {
-return (
-<Fragment>
-<header className={classes.header}>
-    <h1> Yummy Hut </h1>
-    <button>Cart</button>
-</header>
-<div className={classes['main-image']}>
-    <img src ={mealsImage} alt ='A table full of food'></img>
-</div>
-</Fragment>
-);
-}
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Yummy Hut </h1>
+        <HeaderCartButtons onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
